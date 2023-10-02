a88ff94e4e4bc083e91a81f7dbb4b755296bbf7c
Sayantani <88243330+musicallysouled@users.noreply.github.com>
2023-10-02 18:23:14 +0530

    Delete BB_BLOOD_SPECIMEN.sql

--- BB_BLOOD_SPECIMEN.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE bb_blood_specimen(
-	spec_id varchar(10) NOT NULL,
-	b_group varchar(3) NOT NULL,
-	spec_status char(1) NOT NULL Default 'NA',
-
-	Constraint PK001_M0236_0022 PRIMARY KEY (spec_id),
-	Constraint CK001_M0236_0023 CHECK (spec_status IN ('NA','T','F'))
-	);
